6ed7ee1797fbf7c929223bb91bb64afd6c9ea9e1
Dikowiec Sebastian <diko2077@gmail.com>
2022-06-14 15:38:43 -0300

    tests armados

--- CarpetaComposite/EmailComponent.ts
+++
@@ -1,9 +0,0 @@
-export abstract class EmailComponent {
-    public Asunto: string = "";
-    public Contenido: string = "";
-
-    constructor(asunto: string, contenido: string) {
-        this.Asunto = asunto;
-        this.Contenido = contenido;
-    }
-}
