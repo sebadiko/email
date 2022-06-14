903927dcc01fdcfd55c176334c00db557d8c9bcd
Dikowiec Sebastian <diko2077@gmail.com>
2022-06-14 15:41:32 -0300

    Leaf Email

---
+++ CarpetaComposite/EmailLeaf.ts
@@ -0,0 +1,14 @@
+import { Contacto } from './../contacto';
+import { EmailComponent } from './EmailComponent';
+
+export class EmailLeaf extends EmailComponent{
+    public Remitente: Contacto = null;
+    public Para: Array<Contacto> = [];
+
+    constructor(asunto: string, contenido: string, remitente: Contacto, para: Array<Contacto>) {
+        super(asunto, contenido);
+        this.Remitente = remitente;
+        this.Para = para;
+    }
+  
+}
